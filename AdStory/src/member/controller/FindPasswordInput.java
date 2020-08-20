7b9575e8176ec105da3294d517a09959119b0b4f
jmleeh4 <68540905+jmleeh4@users.noreply.github.com>
2020-08-20 17:11:25 +0900

    error correction (#17)

--- AdStory/src/member/controller/FindPasswordInput.java
+++ AdStory/src/member/controller/FindPasswordInput.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/member/findPasswordInput")
 public class FindPasswordInput extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
     /**
      * @see HttpServlet#HttpServlet()
      */
