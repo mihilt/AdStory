3131935aac6614a9003fa6a15578dfa4a47ae8b8
mihilt <50368758+mihilt@users.noreply.github.com>
2020-08-21 20:23:19 +0900

    Added tip board search (#23)

--- AdStory/src/tipboard/controller/TipBoardSearchServlet.java
+++ AdStory/src/tipboard/controller/TipBoardSearchServlet.java
@@ -1,10 +1,11 @@
-package admin.controller;
+package tipboard.controller;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,20 +13,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.Utils;
-import member.model.service.MemberService;
-import member.model.vo.Member;
+import tipboard.model.service.TipBoardService;
+import tipboard.model.vo.TipBoard;
 
 /**
- * Servlet implementation class AdminMemberFinderServlet
+ * Servlet implementation class TipBoardSearchServlet
  */
-@WebServlet("/admin/memberFinder")
-public class AdminMemberFinderServlet extends HttpServlet {
+@WebServlet("/tipBoard/search")
+public class TipBoardSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AdminMemberFinderServlet() {
+    public TipBoardSearchServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -36,38 +37,37 @@ public class AdminMemberFinderServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String searchType = request.getParameter("searchType");
 		String searchKeyword = request.getParameter("searchKeyword");
-		
+
 		int cPage = 1;
-		int numPerPage = 20;
+		int numPerPage = 10;
 		try {
 			cPage = Integer.parseInt(request.getParameter("cPage"));			
 		} catch(NumberFormatException e) {
 			
 		}
-	
+		
 		Map<String, Object> param = new HashMap<>();
 		param.put("searchType", searchType);
 		param.put("searchKeyword", searchKeyword);
 		param.put("cPage", cPage);
 		param.put("numPerPage", numPerPage);
 		
-		List<Member> list = new MemberService().searchMember(param);
-		
-		int totalContents = new MemberService().getTotalContents(param);
+		List<TipBoard> list = new TipBoardService().searchPost(param);
+		int totalContents = new TipBoardService().searchPostCount(param);
 
-		String url = request.getRequestURI() 
-				   + "?searchType=" + searchType
-				   + "&searchKeyword=" + searchKeyword;
-					
-		String pageBar = Utils.getPageBarHtml(cPage, numPerPage, totalContents, url);
+		String url = request.getRequestURI();
 		
-		request.setAttribute("list", list);
-		request.setAttribute("pageBar", pageBar);
-		request.getRequestDispatcher("/WEB-INF/views/admin/memberFinder.jsp")
-			   .forward(request, response);		
+		String pageBar = Utils.getPageBarHtml(cPage, numPerPage, totalContents, url);
+	
+		request.setAttribute("list",list);
+		request.setAttribute("pageBar",pageBar);	
+		RequestDispatcher reqDispatcher = request.getRequestDispatcher("/WEB-INF/views/tipBoard/BoardListSearch.jsp");
+		reqDispatcher.forward(request, response);
 	}
 
-
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
